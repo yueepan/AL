28316080eca205ab8a3f3802ba20a7c00366eb88
yueepan <66990436+yueepan@users.noreply.github.com>
2023-05-16 13:30:03 +0200

    Add files via upload

---
+++ YOLO-NAS.py
@@ -0,0 +1,153 @@
+from super_gradients.training import models
+yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco")
+
+from super_gradients.training import Trainer
+CHECKPOINT_DIR = 'checkpoints'
+trainer = Trainer(experiment_name='my_first_yolonas_run', ckpt_root_dir=CHECKPOINT_DIR)
+
+from super_gradients.training import dataloaders
+from super_gradients.training.dataloaders.dataloaders import coco_detection_yolo_format_train, coco_detection_yolo_format_val
+
+dataset_params = {
+    'data_dir':'/home/panyue/AL/small dataset',
+    'train_images_dir':'train/images',
+    'train_labels_dir':'train/labels',
+    'val_images_dir':'val/images',
+    'val_labels_dir':'val/labels',
+    'test_images_dir':'test/images',
+    'test_labels_dir':'test/labels',
+    'classes': ['airplane', 'helicopter', 'small-vehicle', 'large-vehicle', 'ship', 'container', 'storage-tank','swimming-pool', 'windmill', 'ignore']
+}
+
+from IPython.display import clear_output
+
+train_data = coco_detection_yolo_format_train(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['train_images_dir'],
+        'labels_dir': dataset_params['train_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+val_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['val_images_dir'],
+        'labels_dir': dataset_params['val_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+test_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['test_images_dir'],
+        'labels_dir': dataset_params['test_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+clear_output()
+
+train_data.dataset.transforms
+
+train_data.dataset.plot()
+
+from super_gradients.training import models
+model = models.get('yolo_nas_l', 
+                   num_classes=len(dataset_params['classes']), 
+                   pretrained_weights="coco"
+                   )
+
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training.metrics import DetectionMetrics_050
+from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
+
+train_params = {
+    # ENABLING SILENT MODE
+    'silent_mode': True,
+    "average_best_models":True,
+    "warmup_mode": "linear_epoch_step",
+    "warmup_initial_lr": 1e-6,
+    "lr_warmup_epochs": 3,
+    "initial_lr": 5e-4,
+    "lr_mode": "cosine",
+    "cosine_final_lr_ratio": 0.1,
+    "optimizer": "Adam",
+    "optimizer_params": {"weight_decay": 0.0001},
+    "zero_weight_decay_on_bias_and_bn": True,
+    "ema": True,
+    "ema_params": {"decay": 0.9, "decay_type": "threshold"},
+    # ONLY TRAINING FOR 10 EPOCHS FOR THIS EXAMPLE NOTEBOOK
+    "max_epochs": 10,
+    "mixed_precision": True,
+    "loss": PPYoloELoss(
+        use_static_assigner=False,
+        # NOTE: num_classes needs to be defined here
+        num_classes=len(dataset_params['classes']),
+        reg_max=16
+    ),
+    "valid_metrics_list": [
+        DetectionMetrics_050(
+            score_thres=0.1,
+            top_k_predictions=300,
+            # NOTE: num_classes needs to be defined here
+            num_cls=len(dataset_params['classes']),
+            normalize_targets=True,
+            post_prediction_callback=PPYoloEPostPredictionCallback(
+                score_threshold=0.01,
+                nms_top_k=1000,
+                max_predictions=300,
+                nms_threshold=0.7
+            )
+        )
+    ],
+    "metric_to_watch": 'mAP@0.50'
+}
+
+trainer.train(model=model, 
+              training_params=train_params, 
+              train_loader=train_data, 
+              valid_loader=val_data)
+
+best_model = models.get('yolo_nas_l',
+                        num_classes=len(dataset_params['classes']),
+                        checkpoint_path="checkpoints/my_first_yolonas_run/average_model.pth")
+
+trainer.test(model=best_model,
+            test_loader=test_data,
+            test_metrics_list=DetectionMetrics_050(score_thres=0.1, 
+                                                   top_k_predictions=300, 
+                                                   num_cls=len(dataset_params['classes']), 
+                                                   normalize_targets=True, 
+                                                   post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01, 
+                                                                                                          nms_top_k=1000, 
+                                                                                                          max_predictions=300,                                                                              
+                                                                                                          nms_threshold=0.7)
+
+
+model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
+
+import cv2
+import numpy as np
+
+
+img = cv2.imread('path/to/test.jpg')
+#img = cv2.cvtColor(img, cv2.Color_BGR2RGB)
+
+outputs = best_model.predict(img)
+
+outputs.show()
