793f05a192016b28fa1309f53d205d71d6d402b7
Pan Yue <66990436+yueepan@users.noreply.github.com>
2023-05-26 21:53:35 +0200

    Update YOLO-V8.py

--- YOLO-V8.py
+++ YOLO-V8.py
@@ -8,7 +8,6 @@ ROOT_DIR = "/home/yue/AL"
 #!ls -lt $ROOT_DIR
 
 model = YOLO('yolov8n.yaml')  # build a new model from YAML
-#TOWARDSDS TUTORIAL CODES
 #yolo task=detect mode=train model=yolov8n.pt data=config_file.yaml epochs=3 imgsz=800
 
 results = model.train(data = os.path.join(ROOT_DIR, "config_file.yaml"), epochs = 100)
